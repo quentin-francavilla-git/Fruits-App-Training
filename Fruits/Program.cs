8018afa0225dcf1e59128516fcc70e2637fa5dd6
Quentin Francavilla <quentin.francavilla@gmail.com>
2023-01-23 18:47:07 +0100

    WIP

--- Fruits/Program.cs
+++ Fruits/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//little test
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
